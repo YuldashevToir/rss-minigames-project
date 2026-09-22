adc1d704ccf672dfab091df40c8bb1b34879a3d5
YuldashevToir <reindeerinthedarkforest@gmail.com>
2026-09-22 21:56:43 +0500

    chore: add global scss declaration module to resolve typescript compiler imports

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,1 +1,10 @@
-const empty: string = "just a placeholder string";
+import '../styles/main.scss';
+
+const root = document.createElement('div');
+root.id = 'app-root';
+
+const title = document.createElement('h1');
+title.textContent = 'SPA Minigame Project';
+
+root.append(title);
+document.body.append(root);
